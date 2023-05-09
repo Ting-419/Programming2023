7dca0b7ca619c7778bcb7cc3edcc354dd152d5f3
Ting-419 <longting921@gmail.com>
2023-05-09 14:29:51 +0800

    Lab01

--- Module0120230404/src/routers/self.mjs
+++ Module0120230404/src/routers/self.mjs
@@ -1,28 +1,32 @@
 import express from 'express';
 
-import {
-  getMyInfo,
-  getCurrentTime
-} from '../controllers/self.mjs';
+// import {
+//   getMyInfo,
+//   getCurrentTime
+// } from '../controllers/self.mjs';
 
 const router = express.Router()
 
-// router.get('/myFullNameAndGroup', (req, res) => {
-//   res.send('My name is Ting, my group is KP-14.')
-// })
+router.get('/myFullNameAndGroup', (req, res) => {
+  res.send('My name is Ting, my group is KP-14.')
+})
 
-// router.get('/theCurrentTime', (req, res) => {
-//     res.send({currentTime: new Date().toISOString()})
-//     })
+router.get('/theCurrentTime', (req, res) => {
+    res.send({currentTime: new Date().toISOString()})
+})
+
+router.get('/myNumberOfList', (req, res) => {
+  res.send('My number of list is 01.')
+})
 
 // with controllers
-router.get('/myFullNameAndGroup', (req, res) => {
-  const result = getMyInfo();
-  res.send(result)
-});
-router.get('/CurrentTime', (req, res) => {
- const result = getCurrentTime();
-  res.send(result)
-});
+// router.get('/myFullNameAndGroup', (req, res) => {
+//   const result = getMyInfo();
+//   res.send(result)
+// });
+// router.get('/CurrentTime', (req, res) => {
+//  const result = getCurrentTime();
+//   res.send(result)
+// });
 
 export default router;
